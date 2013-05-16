3593b4ed863cc117e79c2b95c70e90e0233839b1
Sanpi <sanpi@homecomputing.fr>
2013-05-17 01:14:30 +0200

    Generate bootstrap-responsive.php

--- src/Sanpi/TwitterBootstrap/Composer/ScriptHandler.php
+++ src/Sanpi/TwitterBootstrap/Composer/ScriptHandler.php
@@ -40,6 +40,9 @@ class ScriptHandler
         $css = $lessc->compileFile("$bootstrapDir/less/bootstrap.less");
         file_put_contents("$webDir/css/bootstrap.css", $css);
 
+        $css = $lessc->compileFile("$bootstrapDir/less/responsive.less");
+        file_put_contents("$webDir/css/bootstrap-responsive.css", $css);
+
         foreach (glob("$bootstrapDir/js/*.js") as $src) {
             $dst = "$webDir/js/" . basename($src);
             copy($src, $dst);
